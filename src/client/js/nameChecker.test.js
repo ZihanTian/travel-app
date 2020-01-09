39c91c7eb874bf42d0c3fe9829db6a8f6fea9ff9
ZihanTian <zti@umich.edu>
2020-01-08 19:59:38 -0500

    remove unrelated js

--- src/client/js/nameChecker.test.js
+++
@@ -1,6 +0,0 @@
-import{
-    checkForName
-} from "./nameChecker";
-test('It is a valid url',() =>{
-    expect(checkForName('http://techcrunch.com/2015/07/16/microsoft-will-never-give-up-on-mobile')).toBeTruthy()
-}) 
